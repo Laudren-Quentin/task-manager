84c382b737cc419fe2c4ed78378c9661dce2a51f
quentin laudren <laudren.quentin@outlook.fr>
2023-09-03 01:28:57 +0200

    Créer une tâche

--- src/Controller/TaskController.php
+++ src/Controller/TaskController.php
@@ -6,6 +6,7 @@ use App\Entity\Project;
 use App\Entity\Task;
 use App\Form\TaskType;
 use App\Repository\ProjectRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
@@ -19,29 +20,37 @@ class TaskController extends AbstractController
     /**
      * @Route("/project/{id}/create-task", name="app_create_task")
      */
-    public function createTask($id, Request $request, ProjectRepository $projectRepository, UserInterface $user): Response
+    public function createTask($id, Request $request, ProjectRepository $projectRepository, UserInterface $user, EntityManagerInterface $entityManager): Response
     {
         // Récupérez le projet associé à l'ID fourni avec toutes les informations nécéssaire 
-        $project = $projectRepository->findProjectByIdWithTeam($id);
-        $project = $project[0];
+        $project = $projectRepository->findProjectById($id);
         VarDumper::dump($project);
 
         // Créez une nouvelle tâche liée au projet
         $task = new Task();
         $task->setProject($project);
         $task->setCreator($user); // Le créateur de la tâche est l'utilisateur connecté
+        $task->setCompleted(false);
 
         $form = $this->createForm(TaskType::class, $task);
         $form->handleRequest($request);
         VarDumper::dump($form);
-
+        
         if ($form->isSubmitted() && $form->isValid()) {
+            
+             // Set createdAt and updatedAt
+             $now = new \DateTimeImmutable();
+             $task->setCreatedAt($now);
+             $task->setUpdatedAt($now);
+ 
+             $entityManager->persist($task);
+             $entityManager->flush();
+
             return $this->redirectToRoute('app_project_details', ['id' => $id]);
         }
 
         return $this->render('task/create.html.twig', [
             'form' => $form->createView(),
-            'project' => $project,
         ]);
     }
 }
--- src/Form/TaskType.php
+++ src/Form/TaskType.php
@@ -2,19 +2,36 @@
 
 namespace App\Form;
 
+use App\Entity\Project;
 use App\Entity\Task;
+use App\Repository\ProjectRepository;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class TaskType extends AbstractType
 {
+    private $projectRepository;
+
+    public function __construct(ProjectRepository $projectRepository)
+    {
+        $this->projectRepository = $projectRepository;
+    }
+
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
+       $project = $options['data']->getProject(); // Obtenez le projet de la tâche
+
         $builder
             ->add('title')
             ->add('description')
-        ;
+            ->add('assignedUser', ChoiceType::class, [
+                'choices' => $this->getUserChoices($project),
+                'required' => false,
+                'placeholder' => 'choisir un utilisateur',
+                'label' => 'Utilisateur assigné : ',
+            ]);
     }
 
     public function configureOptions(OptionsResolver $resolver): void
@@ -22,5 +39,16 @@ class TaskType extends AbstractType
         $resolver->setDefaults([
             'data_class' => Task::class,
         ]);
+    }
+
+    private function getUserChoices(Project $project)
+    {
+        $userChoices = [];
+
+        foreach ($project->getTeam() as $user) {
+            $userChoices[$user->getEmail()] = $user;
+        }
+
+        return $userChoices;
     }
 }
